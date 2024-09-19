e7257450c0f292d2cd8bbe76272b3e442b4a8f84
mwszc <cai.lih@northeastern.edu>
2024-09-18 17:38:37 -0700

    final

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,9 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+      <h2>Name: Lihan Cai</h2>
+      <h2>Section: SEC 01 Fall 2024</h2>
+      <a href="https://github.com/LihanCai/kanbas-react-web-app" target="_blank">Lihan Cai's Source Code Repository</a>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
